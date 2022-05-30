24c822815fbb1d087a3fb631e2ff4eef5554bd53
FlutLab.io <>
2022-05-30 04:06:38 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BelajarNavBar(),
+    );
+  }
+}
+
+class BelajarNavBar extends StatefulWidget {
+  @override
+  _BelajarNavBarState createState() => _BelajarNavBarState();
+}
+
+class _BelajarNavBarState extends State<BelajarNavBar> {
+  int _selectedNavbar = 0;
+  var _pages = <Widget>[
+    Icon(Icons.home, size: 210, color: Colors.white),
+    Icon(Icons.assignment, size: 210, color: Colors.white),
+    Icon(Icons.person, size: 210, color: Colors.white),
+  ];
+
+  void _changeSelectedNavBar(int index) {
+    setState(() {
+      _selectedNavbar = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Roy Phone"),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(child: _pages.elementAt(_selectedNavbar)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: ('Beranda'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            label: ('Pesanan'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: ('Akun'),
+          ),
+        ],
+        currentIndex: _selectedNavbar,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        onTap: _changeSelectedNavBar,
+      ),
+    );
+  }
+}
